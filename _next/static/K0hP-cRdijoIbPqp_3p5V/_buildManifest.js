c5d52849aeff57313810bc4d87eb99bb501b5ea2
Cayke Daniel <caykedanielcd@gmail.com>
2025-12-04 17:32:37 -0300

    Updates

---
+++ _next/static/K0hP-cRdijoIbPqp_3p5V/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-26bf0e9fbd67e4bd.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
